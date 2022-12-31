72541948dd66cacb1cc60d520cfed98ab0d0ad5e
metall13 <metall1390@gmail.com>
2022-12-31 18:25:50 +0300

    refactoring db client

--- internal/db/client.go
+++ internal/db/client.go
@@ -10,17 +10,17 @@ import (
 
 var DB *sqlx.DB
 
-func ConnectDB(dsn string) (*sqlx.DB, error) {
-	db, err := sqlx.Connect("postgres", dsn)
+func ConnectDB(dsnDb string) (*sqlx.DB, error) {
+	var err error
+	DB, err = sqlx.Connect("postgres", dsnDb)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	err = db.Ping()
+	err = DB.Ping()
 	if err != nil {
 		fmt.Println("Databese not connect")
 		return DB, err
 	}
-	var DB = db
 	return DB, nil
 }
