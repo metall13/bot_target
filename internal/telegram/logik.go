38e4109e06007b1baa16b20904fa58a08ef68817
metall13 <metall1390@gmail.com>
2022-12-31 18:17:31 +0300

    add relise 0.0.1

--- internal/telegram/logik.go
+++ internal/telegram/logik.go
@@ -13,6 +13,7 @@ func GetTargetAvtoToGosNomer(message string, db *sqlx.DB) (string, error) {
 
 	//phoneRegexpNombeAvto := regexp.MustCompile(`^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$`)
 	//phoneNumberNombeAvto := phoneRegexpNombeAvto.FindString(message)
+
 	qwery := `SELECT       
     fio,          
     place_birth, 
